15cfac4f27a3b245a86add776f9957e7d5ea23b7
joshua enokson <kilograhm@pm.me>
2021-05-12 01:53:15 -0700

    added i3 stuff

---
+++ i3status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script to show current keyboard layout in i3status bar
+# Requires https://github.com/nonpop/xkblayout-state
+
+# Inspiration for wather display:
+# https://keheliya.blogspot.com/2018/01/weather-info-in-i3status.html
+# Suggested crontab entry:
+# */5 * * * * curl -s wttr.in/?format=3\&m > ~/.weather.cache
+
+# For Purple air updates:
+# */5 * * * * ~/bin/purple.py > /dev/null
+
+i3status --config /home/joshua/.config/i3status/config | while :
+do
+	read line
+	echo "$(xkb-switch) | $line" || exit 1
+done
+
